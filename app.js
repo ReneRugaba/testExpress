a5433c8cccb81835c269a5ad236cd14647d9dbc7
rugaba <rugaba@gmail.com>
2021-05-25 22:20:33 +0200

    add Router

--- app.js
+++ app.js
@@ -3,8 +3,7 @@ const express=require("express");
 const morgan = require("morgan");
 const {success,error}= require("functionStatus")
 const app= express()
-const APP_PUT="/api/v1/members/:id"
-const APP_VERSION="/api/v1/members"
+
 
 
 const members=[
@@ -22,32 +21,35 @@ const members=[
     }
 ]
 app.use(morgan("dev"))
-
+let MembersRouter= express.Router()
+let MembersRouterId=express.Router()
 app.use(express.json()) 
 app.use(express.urlencoded({ extended: true }))
 
-app.get(APP_VERSION,(req,res)=>{
+MembersRouter.route("/")
+    .get((req,res)=>{
     
    if (req.query.max && req.query.max > 0) {
     res.send(members.slice(0,req.query.max))
    }else{
     res.send(members)
    }
-})
+    })
 
-app.post(APP_VERSION,(req,res)=>{
+    .post((req,res)=>{
    if (req.body.name!="") {
        members.push({
            id:members.length+1,
            name:req.body.name
        })
-    res.status(201).json(success(members))
-   }else{
-    res.status(401).json(error("body vide!"))
-   }
-})
+        res.status(201).json(success(members))
+    }else{
+        res.status(401).json(error("body vide!"))
+    }
+    })
 
-app.put(APP_PUT,(req,res)=>{
+MembersRouter.route("/:id")
+    .put((req,res)=>{
     if (req.body.name!="" && req.params.id) {
         let indexMembers=""
         for (let index = 1; index < members.length; index++) {
@@ -62,6 +64,7 @@ app.put(APP_PUT,(req,res)=>{
         res.status(401).json(error("body vide!"))
     }
 })
+app.use("/api/v1/members",MembersRouter)
 
 app.listen(8080,()=>{
     console.log("app started!")
